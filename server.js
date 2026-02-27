1b447cdaed0f57543b2e15f2f246e28452ec22d3
Manthan Sachdev <156173895+manthansachdev12@users.noreply.github.com>
2026-02-27 06:52:32 +0530

    Refactor server.js for cleaner code and organization

--- server.js
+++ server.js
@@ -9,33 +9,25 @@ import identityRoutes from "./routes/identityRoutes.js";
 import applicationRoutes from "./routes/applicationRoutes.js";
 import teamRoutes from "./routes/teamRoutes.js";
 
-
 dotenv.config();
 
-const app=express();
+const app = express();
 
-
+app.use(express.json());
 
 app.use(cors({
   origin: "https://hacksphere-one.vercel.app",
   credentials: true
 }));
 
-app.use(express.json());
-
 connectDB();
 
-app.use("/api/auth",authRoutes);
-app.use("/api/applications",applicationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/applications", applicationRoutes);
+app.use("/api/hackathons", hackathonRoutes);
+app.use("/api/identity", identityRoutes);
+app.use("/api/team", teamRoutes);
 
-app.use("/api/hackathons",hackathonRoutes);
-app.use("/api/identity",identityRoutes);
-app.use("/api/team",teamRoutes);
-
-
-
-app.listen(5000,()=>{
-
- console.log("Server Running");
-
+app.listen(5000, () => {
+  console.log("Server Running");
 });
